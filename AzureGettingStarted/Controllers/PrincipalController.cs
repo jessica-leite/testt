3e1322c29e4def9fb982d9071cb0a344b1e6aeb5
Jessica Leite <jessica.mleite@gmail.com>
2020-08-06 10:11:07 -0300

    PrincipalController

---
+++ AzureGettingStarted/Controllers/PrincipalController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureGettingStarted.Controllers
+{
+    public class PrincipalController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
